baca4862a0dad67c6a8565894cbdc98976a0c247
阿部直也 <n.abe@gemcook.com>
2020-08-02 11:00:43 +0900

    Added api_user urls.

--- app/api_user/urls.py
+++ app/api_user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_user import views
 
 app_name = 'user'
 
 router = DefaultRouter()
+router.register('profile', views.ProfileViewSet)
+router.register('approval', views.FriendRequestViewSet)
 
 urlpatterns = [
+    path('signup/', views.CreateUserView.as_view(), name='signup'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
     path('', include(router.urls))
 ]

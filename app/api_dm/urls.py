6ccddd3b70d8f96fe4b61fc5f1820a58c1385d1b
阿部直也 <n.abe@gemcook.com>
2020-08-02 11:04:55 +0900

    Added api_dm urls.

--- app/api_dm/urls.py
+++ app/api_dm/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_dm import views
 
 app_name = 'dm'
 
 router = DefaultRouter()
+router.register('message', views.MessageViewSet)
+router.register('inbox', vires.InboxListView)
 
 urlpatterns = [
     path('', include(router.urls))

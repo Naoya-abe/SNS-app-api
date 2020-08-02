ac9728445fd71eab0be6048333906a3395506acd
阿部直也 <n.abe@gemcook.com>
2020-08-02 10:53:26 +0900

    Added post_api views.

--- app/api_post/views.py
+++ app/api_post/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets, authentication, permissions, status
+from core import custompermissions
+from core.models import Post
+from api_post import serializers
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (
+        permissions.IsAuthenticated,
+        custompermissions.PostPermission
+    )
+
+    def perform_create(self, serializer):
+        return serializer.save(postFrom=self.request.user)
--- app/core/custompermissions.py
+++ app/core/custompermissions.py
@@ -7,3 +7,11 @@ class ProfilePermission(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.userPro.id == request.user.id
+
+
+class PostPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.postFrom.id == request.user.id

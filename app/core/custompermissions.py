c0a5a4531e860e0443ff3f02d1d263ed95475e60
阿部直也 <n.abe@gemcook.com>
2020-08-14 15:29:06 +0900

    Added comments.

--- app/core/custompermissions.py
+++ app/core/custompermissions.py
@@ -13,8 +13,10 @@ class UpdateOwnProfile(permissions.BasePermission):
 
 
 class PostPermission(permissions.BasePermission):
+    """Allow user to edit their own posts"""
 
     def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own posts"""
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.postFrom.id == request.user.id
